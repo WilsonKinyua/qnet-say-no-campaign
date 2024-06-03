921bcc51e4c5314eb7f8178e49a80fc402bb1b0e
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-06-03 23:59:46 +0300

    Update layout.tsx with new dropdown me nu component for language selection. Enhances user accessibility and navigation.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from "next";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { Roboto } from "next/font/google";
 import "../styles/globals.scss";
 import Footer from "@/components/footer";
-
+import { Languages } from "lucide-react";
+import Link from "next/link";
 const inter = Roboto({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,8 +28,28 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <div className="bg-orange py-2 text-white">
-          <div className="container mx-auto flex justify-center">
-            <p className="text-xs"><a target="_blank" href="https://wa.me/+233256630005" className="font-black">Report Violations at Whatsapp Hotline: <span className="font-black">+ 233 256630005</span></a></p>
+          <div className="container mx-auto flex justify-between items-center">
+            <p className="lg:text-base text-xs"><a target="_blank" href="https://wa.me/+233256630005" className="font-black">Report Violations at Whatsapp Hotline: <span className="font-black">+ 233 256630005</span></a></p>
+            <DropdownMenu>
+              <DropdownMenuTrigger
+                className="border-2 border-white rounded-full p-2 main-gradient"
+              >
+                <Languages className="h-6" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>
+                  <Link href="/">
+                    English
+                  </Link>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link href="/fr">
+                    French
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
         {children}
