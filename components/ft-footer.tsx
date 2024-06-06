e9ac374aa3a95caf8689acc93a105f4289260638
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-06-06 11:02:13 +0300

    Update language dropdown menu in layout for improved navigation accessibility.

--- components/ft-footer.tsx
+++ components/ft-footer.tsx
@@ -41,22 +41,22 @@ export default function Footer() {
                     <div className="lg:flex justify-between items-center py-5">
                         <ul className="lg:flex grid grid-cols-2 lg:flex-wrap lg:space-x-5">
                             <li>
-                                <Link href="/#empowering" className="hover:text-orange transition duration-300">About Us</Link>
+                                <Link href="/fr/#empowering" className="hover:text-orange transition duration-300">À propos de nous</Link>
                             </li>
                             <li>
-                                <Link href="/faqs" className="hover:text-orange transition duration-300">FAQs</Link>
+                                <Link href="/fr/faqs" className="hover:text-orange transition duration-300">FAQs</Link>
                             </li>
                             <li>
-                                <Link href="/media-center" className="hover:text-orange transition duration-300">Media Centre</Link>
+                                <Link href="/fr/media-center" className="hover:text-orange transition duration-300">Galerie</Link>
                             </li>
                             <li>
-                                <Link href="/resources" className="hover:text-orange transition duration-300">Resources</Link>
+                                <Link href="/fr/resources" className="hover:text-orange transition duration-300">Ressources</Link>
                             </li>
                             <li>
-                                <Link href="/insights" className="hover:text-orange transition duration-300">NewsRoom</Link>
+                                <Link href="/fr/insights" className="hover:text-orange transition duration-300">Publications</Link>
                             </li>
                             <li>
-                                <Link href="/contact" className="hover:text-orange transition duration-300">Contact Us</Link>
+                                <Link href="/fr/contact" className="hover:text-orange transition duration-300">Contactez-nous</Link>
                             </li>
                         </ul>
                         <div className="flex items-center space-x-3 lg:mt-0 mt-5">
@@ -74,14 +74,6 @@ export default function Footer() {
                     <hr className="my-2 border-0 border-t-[0.01rem] border-gray-300" />
                     <div className="text-center uppercase py-5">
                         <p>&copy; {new Date().getFullYear()} <Link href="/" className="hover:text-orange transition duration-300" rel="noopener noreferrer">QNET&apos;s Say NO! Campaign</Link>. All Rights Reserved</p>
-                        {/* <ul className="lg:flex hidden">
-                            <li className="lg:border-r border-white mr-5 pr-5">
-                                <Link href="" className="hover:text-orange transition duration-300">Terms & Conditions</Link>
-                            </li>
-                            <li>
-                                <Link href="" className="hover:text-orange transition duration-300">Privacy Statement & Disclaimer</Link>
-                            </li>
-                        </ul> */}
                     </div>
                 </div>
             </footer>
